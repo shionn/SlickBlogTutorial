f3890e1085aa2e188774fe553dff4cedbb08c237
Ludovic KEPKA <ludovic.kepka@ca-titres.fr>
2014-01-09 14:08:34 +0100

    lesson 3

--- src/main/java/lesson3/CharacterGame.java
+++ src/main/java/lesson3/CharacterGame.java
@@ -9,6 +9,7 @@ package lesson3;
 import org.newdawn.slick.Animation;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
@@ -30,7 +31,8 @@ public class CharacterGame extends BasicGame {
     private GameContainer container;
     private TiledMap map;
 
-    private int direction = 0;
+    private float x = 300, y = 300;
+    private int direction = 2;
     private boolean moving = false;
     private Animation[] animations = new Animation[8];
 
@@ -47,15 +49,15 @@ public class CharacterGame extends BasicGame {
         this.container = container;
         this.map = new TiledMap("map/exemple.tmx");
 
-        SpriteSheet spriteSheet = new SpriteSheet("sprite/character.png", 576 / 9, 256 / 4);
-        animations[0] = loadAnimation(spriteSheet, 0, 1, 0);
-        animations[1] = loadAnimation(spriteSheet, 0, 1, 1);
-        animations[2] = loadAnimation(spriteSheet, 0, 1, 2);
-        animations[3] = loadAnimation(spriteSheet, 0, 1, 3);
-        animations[4] = loadAnimation(spriteSheet, 1, 9, 0);
-        animations[5] = loadAnimation(spriteSheet, 1, 9, 1);
-        animations[6] = loadAnimation(spriteSheet, 1, 9, 2);
-        animations[7] = loadAnimation(spriteSheet, 1, 9, 3);
+        SpriteSheet spriteSheet = new SpriteSheet("sprites/character.png", 64, 64);
+        this.animations[0] = loadAnimation(spriteSheet, 0, 1, 0);
+        this.animations[1] = loadAnimation(spriteSheet, 0, 1, 1);
+        this.animations[2] = loadAnimation(spriteSheet, 0, 1, 2);
+        this.animations[3] = loadAnimation(spriteSheet, 0, 1, 3);
+        this.animations[4] = loadAnimation(spriteSheet, 1, 9, 0);
+        this.animations[5] = loadAnimation(spriteSheet, 1, 9, 1);
+        this.animations[6] = loadAnimation(spriteSheet, 1, 9, 2);
+        this.animations[7] = loadAnimation(spriteSheet, 1, 9, 3);
     }
 
     private Animation loadAnimation(SpriteSheet spriteSheet, int startX, int endX, int y) {
@@ -69,6 +71,9 @@ public class CharacterGame extends BasicGame {
     @Override
     public void render(GameContainer container, Graphics g) throws SlickException {
         this.map.render(0, 0);
+        g.setColor(new Color(0, 0, 0, .5f));
+        g.fillOval(x - 16, y - 8, 32, 16);
+        g.drawAnimation(animations[direction + (moving ? 4 : 0)], x - 32, y - 60);
     }
 
     @Override
@@ -77,9 +82,31 @@ public class CharacterGame extends BasicGame {
 
     @Override
     public void keyReleased(int key, char c) {
+        this.moving = false;
         if (Input.KEY_ESCAPE == key) {
             this.container.exit();
         }
     }
 
+    @Override
+    public void keyPressed(int key, char c) {
+        switch (key) {
+            case Input.KEY_UP:
+                this.direction = 0;
+                this.moving = true;
+                break;
+            case Input.KEY_LEFT:
+                this.direction = 1;
+                this.moving = true;
+                break;
+            case Input.KEY_DOWN:
+                this.direction = 2;
+                this.moving = true;
+                break;
+            case Input.KEY_RIGHT:
+                this.direction = 3;
+                this.moving = true;
+                break;
+        }
+    }
 }
